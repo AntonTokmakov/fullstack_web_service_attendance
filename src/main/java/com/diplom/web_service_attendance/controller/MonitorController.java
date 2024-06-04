7c18eb76346233db44b7eb12818de1e5091dcaa1
Anton <antontokmakov757@gmail.com>
2024-06-04 15:37:33 +0700

    End PD

--- src/main/java/com/diplom/web_service_attendance/controller/MonitorController.java
+++ src/main/java/com/diplom/web_service_attendance/controller/MonitorController.java
@@ -38,7 +38,7 @@ public class MonitorController {
     public String getLessonGroupAndWeekday(Principal principal,
                                            Model model){
         List<CheckActualLesson> checkActualLessons = new ArrayList<>();
-        LocalDate date = LocalDate.now();//                             .minusWeeks(5);   //todo - delete minusWeeks
+        LocalDate date = LocalDate.of(2024, 4, 9); //LocalDate.now();//      todo - delete
         try {
             List<ActualLesson> lessonList = actualLessonService.findActualLessonByDateAndStudy(date, principal.getName());
 
